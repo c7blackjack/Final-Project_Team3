1f5a062495988c0a97a79938ac74ed34afa33ab0
TravisLloyd <lloydtravis@gmail.com>
2021-04-17 09:39:54 -0700

    Changed the subset to achieve higher rsquared. cleaned up code and added a 3dscatterplot

--- Final_Project.R
+++ Final_Project.R
@@ -25,6 +25,11 @@ data$sqft_lot[which(is.na(data$sqft_lot))] <- 6500
 ##Use data2 from her because it has less columns. I left data alone to retain its columns
 data2 <- subset(data, select = c(price, bedrooms, bathrooms, sqft_living, sqft_lot, waterfront, view))
 head(data2)
+data.cor <- subset(data, select = -c(date))
+head(data.cor)
+cor(data.cor)
+
+
 #missing data is filled in.
 ##Moving forward we will look at the outliers of the main categories that we will be inspecting.
 boxplot(data2$bathrooms)
@@ -37,9 +42,6 @@ boxplot(data2$price)
 ##outliers > 1000000
 boxplot(data2$sqft_lot)
 
-boxplot(data$grade)
-##outliers >9 and less than 6
-
 
 #checking and removing outliers and putting back into data2
 data2 <- subset(data2, bedrooms <=5 
@@ -47,68 +49,51 @@ data2 <- subset(data2, bedrooms <=5
                        & sqft_living < 3500 
                        & bathrooms < 3.5 
                        & price <= 900000
-                       & sqft_lot <=13000
-                       & grade <= 9 
-                       & grade >=6 )
+                       & sqft_lot <=13000)
 ##Making a scaled version of data2 to use in linear regression models
 data2.scaled <- scale(data2)
+cor(data)
+data2.df <- as.data.frame(data2.scaled)
+typeof(data2.df)
+
 head(data2.scaled)
 boxplot(data2$sqft_lot)
 boxplot(data2$bathrooms)
 boxplot(data2$bedrooms)
 boxplot(data2$sqft_living)
 boxplot(data2$price)
-boxplot(data$grade)
+
+
+data3 <- subset(data, select = c(price, bathrooms, sqft_living, grade))
+cor(data3)
+boxplot(data3$grade)
+data3 <- subset(data3, sqft_living < 3500 
+                & bathrooms < 3.5 
+                & price <= 900000
+                & grade >= 6
+                & grade <= 9)
+#data3.scaled <- scale(data3)
+data3.df <- as.data.frame(data3)
 
 ##data$bathrooms <- data$bathrooms(is.na(data$bathrooms))
 ggplot(data2, aes(x=sqft_living, y=price, color = bedrooms)) + geom_point()
 ggplot(data2, aes(x = sqft_living, y = (price/10000), color = grade)) + geom_point()
-data.scaled.price <- scale(price)
-data.scaled.sqft_living <- scale(sqft_living)
-model1 <- lm(price ~ sqft_living + bedrooms, data = data2.scaled)
+
+##Linear Models for data3
+model1 <- lm(price ~ grade, data = data3.df)
+model2 <- lm(price ~ sqft_living + bathrooms, data = data3.df)
+model3 <- lm(price ~ sqft_living + bathrooms + grade, data = data3.df)
 summary(model1)
-
-#testing correlation between variables
-
-# correlation between price and sqft_living
-cor_1 <- cor.test(data$price, data$sqft_living, 
-                  method = "pearson")
-cor_1
-sprintf("The correlation Index between price and sqft living is %f" , cor_1$estimate)
-
-#Both have moderate correlation 
-
-#correlation  Index between price and sqft_lot
-cor_2 <- cor.test(data$price, data$sqft_lot, 
-                  method = "pearson")
-cor_2
-sprintf("The correlation Index between price and sqft lot is %f" , cor_2$estimate)
-
-#very less correlation almost 0.
-
-# Now testing correlation between price and condition.
-cor_3 <- cor.test(data$price, data$condition, 
-                  method = "pearson")
-cor_3
-sprintf("The correlation Index between price and condition is %f" , cor_3$estimate)
-
-#correlation between price and yr_built.
-cor_4 <- cor.test(data$price, data$yr_built, 
-                  method = "pearson")
-cor_4
-sprintf("The correlation Index between price and yr_built  is %f" , cor_4$estimate)
-ggplot(data, aes(price, yr_built, color= bathrooms)) + geom_point()
-ggplot(data, aes(price, yr_renovated, color= bathrooms)) + geom_point()
-
-#Boxplot to check outliers in grade 
-#boxplot(data$grade)
+summary(model2)
+summary(model3)
+plot(model1)
 
 
-#correlation  between price and grade
-cor_5 <- cor.test(data$price, data$grade, 
-                  method = "pearson")
-cor_5
-ggplot(data, aes(price, grade , color= bedrooms)) + geom_point()
-#boxplot(data$grade)
+require(scatterplot3d)
+library(nat)
+library(rgl)
+plot3d(data3[1:5000,1:3], )
+with(iris, plot3)
+?scatterplot3d
 
-
+?lm
